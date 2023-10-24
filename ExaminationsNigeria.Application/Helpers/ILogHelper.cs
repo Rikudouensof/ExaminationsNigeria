313dce4708c78ea5dcdf47885e6618ae099c688b
Rikudou En Sof <info@rikudouensof.com>
2023-10-24 17:54:52 +0100

    Install NLog package, Add Log Helper and Test file log

---
+++ ExaminationsNigeria.Application/Helpers/ILogHelper.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ExaminationsNigeria.Application.Helpers
+{
+  public interface ILogHelper
+  {
+    void LogError(string requestId, string message, string ip, string methodName, Exception ex);
+    void LogInformation(string requestId, string message, string ip, string methodName);
+    void logInformation(string requestId, string message, string ip, string methodName, object arguemnet);
+    void LogTrace(string requestId, string message, string ip, string methodName, Exception ex);
+    void logWarning(string requestId, string message, string ip, string methodName);
+  }
+}
